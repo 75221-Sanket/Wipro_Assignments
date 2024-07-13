5bff20e998030b6d2891b1d6710e448042e7ceb4
75221-Sanket <128774766+75221-Sanket@users.noreply.github.com>
2024-07-13 23:56:20 +0530

    Add files via upload

---
+++ Assignment_Day5/Day5-2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class program
+{
+    static int sumAlternateNum(int number)
+    {
+        int sum =0;
+        int position = 1;
+
+        while (number>0)
+        {
+            int digit = number % 10;
+            if (position % 2 !=0)
+            {
+                sum+= digit;
+            }
+            number /= 10;
+            position++;
+        }
+        return sum;
+    }
+    static void Main()
+    {
+        int number = 12345;
+        Console.WriteLine("Input" +number);
+
+        int sum = sumAlternateNum(number);
+
+        Console.WriteLine("Answer" +sum);
+    }
+}
---
+++ Assignment_Day5/Day5-3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class program
+{
+
+    static void PrintWords(string input)
+    {
+        string word = "";
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == ' ')
+            {
+                Console.WriteLine(word);
+                word = "";
+            }
+            else
+            {
+                word += input[i];
+            }
+        }
+        Console.WriteLine(word);
+
+    }
+    static void Main()
+    {
+        string input = "welcome to C# Programmin";
+        Console.WriteLine("Input" + input);
+        Console.WriteLine("Output");
+
+        PrintWords(input);
+    }
+
+}
