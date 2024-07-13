0492db3e0ce479a6c2ad16491566fe516b5fdc1d
75221-Sanket <128774766+75221-Sanket@users.noreply.github.com>
2024-07-13 23:55:22 +0530

    Add files via upload

---
+++ Day5-1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Admin
+{
+    class Admin
+    {
+        static bool lsValidUser(string username, string password)
+        {
+            if (username.Trim() == "admin" && password.Trim() == "admin123")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            static void Main(string[] args)
+            {
+                int count = 0;
+                do
+                {
+                    Console.WriteLine("Enter the username :");
+                    string username = Console.ReadLine();
+
+                    Console.WriteLine("Enter password");
+                    string password = Console.ReadLine();
+
+                    bool result = lsValidUser(username, password);
+
+                    if (result)
+                    {
+                        Console.WriteLine("Welcome to {0}", username);
+                        break;
+                    }
+                    else
+                    {
+                        count++;
+                        Console.WriteLine("Invalid User Id or Password. Wrong Attempts count " + count);
+
+                        if (count == 3)
+                        {
+                            Console.WriteLine("you account is locked");
+                            break;
+                        }
+                    }
+
+
+                } while (count < 3);
+
+                Console.ReadLine();
+            }
+            }
+        }
+    }
