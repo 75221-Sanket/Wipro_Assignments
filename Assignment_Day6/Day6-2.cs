64911094a390d9cecde8667b1e15431ecc600d1c
75221-Sanket <128774766+75221-Sanket@users.noreply.github.com>
2024-07-13 23:57:52 +0530

    Add files via upload

---
+++ Assignment_Day6/Day6-2.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        int length = numbers.Length;
+
+        for (int i = 0; i < length / 2; i++)
+        {
+            int temp = numbers[i];
+            numbers[i] = numbers[length - i - 1];
+            numbers[length - i - 1] = temp;
+        }
+
+        Console.WriteLine("Reversed array: " + string.Join(", ", numbers));
+    }
+}
