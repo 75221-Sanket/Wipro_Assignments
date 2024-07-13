1d8d773daf35765f685f42823f51e622473593f0
75221-Sanket <128774766+75221-Sanket@users.noreply.github.com>
2024-07-13 23:56:50 +0530

    Add files via upload

---
+++ Day6-1.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 3, 5, 7, 2, 8, 10, 6 };
+        int first = int.MinValue;
+        int second = int.MinValue;
+
+        foreach (int num in numbers)
+        {
+            if (num > first)
+            {
+                second = first;
+                first = num;
+            }
+            else if (num > second && num != first)
+            {
+                second = num;
+            }
+        }
+
+        Console.WriteLine("The second largest element is: " + second);
+    }
+}
