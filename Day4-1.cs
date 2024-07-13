33211d3bec5c7cdf80e21dc6ea4a68fc5ab8d612
75221-Sanket <128774766+75221-Sanket@users.noreply.github.com>
2024-07-13 23:53:14 +0530

    Add files via upload

---
+++ Day4-1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class IntToChr
+{
+    static void Main()
+    {
+        Console.Write("Enter a number");
+        int number = Convert.ToInt32 (Console.ReadLine());
+
+        string[] words = { "zero", "one", "two", "Three", "four", "five", "six", "seven", "eight", "nine" };
+
+        string resurt =
+    }
+}
