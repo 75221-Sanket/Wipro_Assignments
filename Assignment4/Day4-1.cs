9f3e570b7d9ff5a8bb2d50a53e37f0e8295a5c4c
75221-Sanket <128774766+75221-Sanket@users.noreply.github.com>
2024-07-15 09:10:25 +0530

    Update Day4-1.cs

--- Assignment4/Day4-1.cs
+++ Assignment4/Day4-1.cs
@@ -1,14 +1,35 @@
-﻿using System;
+using System;
 
-class IntToChr
+class Program
 {
     static void Main()
     {
-        Console.Write("Enter a number");
-        int number = Convert.ToInt32 (Console.ReadLine());
+        Console.Write("Enter a number: ");
+        string input = Console.ReadLine();
+        
+        string result = ConvertNumberToWords(input);
+        
+        Console.WriteLine("Output: " + result);
+    }
 
-        string[] words = { "zero", "one", "two", "Three", "four", "five", "six", "seven", "eight", "nine" };
-
-        string resurt =
+    static string ConvertNumberToWords(string number)
+    {
+        string[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        
+        string result = "";
+        foreach (char digit in number)
+        {
+            if (char.IsDigit(digit))
+            {
+                int num = digit - '0';
+                result += words[num] + " ";
+            }
+            else
+            {
+                return "Invalid input!";
+            }
+        }
+        
+        return result.Trim();
     }
 }
