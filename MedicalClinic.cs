98ae2c03c16392b969127588d3e9c694f6664279
75221-Sanket <128774766+75221-Sanket@users.noreply.github.com>
2024-07-19 12:39:13 +0530

    Add files via upload

---
+++ MedicalClinic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MedicalClinic
+{
+    class PatientDetails
+    {
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+        public string PatientLastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public bool Haslnsurance { get; set; }
+        public double Weight { get; set; }
+
+        public DateTime LastVisitDate { get; set; }
+        public string ContactNumber { get; set; }
+        public void DisplayPatientDetails()
+        {
+            Console.WriteLine($"Patient Id : {PatientId}");
+            Console.WriteLine($"Patient Name : {PatientName}");
+            Console.WriteLine($"Patient LastName : {PatientLastName}");
+            Console.WriteLine($"Patient Age : {Age}");
+            Console.WriteLine($"Patient Gender : {Gender}");
+            Console.WriteLine($"Patient Haslnsuran : {Haslnsurance}");
+            Console.WriteLine($"Patient Weight : {Weight}");
+            Console.WriteLine($"Patient Last VisitDate : {LastVisitDate}");
+            Console.WriteLine($"Patient ContectNumber : {ContactNumber}");
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            PatientDetails patient = new PatientDetails
+            {
+                PatientId = 100,
+                PatientName = "Sandy",
+                PatientLastName = "Yadli",
+
+                Age = 25,
+                Gender = "Male",
+                Haslnsurance = true,
+                Weight = 70,
+                LastVisitDate = DateTime.Now.AddMonths(-1),
+                ContactNumber = " 3456789012",
+
+            };
+            patient.DisplayPatientDetails();
+
+        }
+    }
+}
+
---
+++ PatientDetalis.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class Customer
+{
+    public int CustomerId { get; set; }
+    public string CustomerName { get; set; }
+
+    public string CustomerCity { get; set; }
+
+    public Customer()
+    {
+        CustomerId = 0;
+        CustomerName = "";
+        CustomerCity = "";
+    }
+
+    public Customer(int id)
+    {
+        CustomerId = id;
+        CustomerName = "";
+        CustomerCity = "";
+    }
+    public Customer(int id, string name)
+    {
+        CustomerId = id;
+        CustomerName = name;
+        CustomerCity = "";
+    }
+    public Customer(int id, string name, string city)
+    {
+        CustomerId = id;
+        CustomerName = name;
+        CustomerCity = city;
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            Customer c2 = new Customer(10256);
+            Customer c3 = new Customer(10256, "Scott");
+            Customer c4 = new Customer(10256, "Scott", "Hyd");
+
+            Console.WriteLine("Customer Details : Id : {0}, name : {1}, city : {2}",c1.CustomerId,c1.CustomerName,c1.CustomerCity);
+            Console.WriteLine("Customer Details : Id : {0}, name : {1}, city : {2}",c2.CustomerId,c2.CustomerName,c2.CustomerCity);
+            Console.WriteLine("Customer Details : Id : {0}, name : {1}, city : {2}",c3.CustomerId,c3.CustomerName,c3.CustomerCity);
+            Console.WriteLine("Customer Details : Id : {0}, name : {1}, city : {2}", c4.CustomerId, c4.CustomerName, c4.CustomerCity);
+
+            Console.ReadLine();
+        
+        }
+    }
+}
