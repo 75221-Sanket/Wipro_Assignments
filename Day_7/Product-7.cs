da8580e7ba38be1af2711f69e5411c743d7bd59c
75221-Sanket <128774766+75221-Sanket@users.noreply.github.com>
2024-07-19 12:41:29 +0530

    Add files via upload

---
+++ Day_7/Product-7.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Product
+{
+    class product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public int Qty { get; set; }
+
+        public product(int productid)
+        {
+            ProductId = productid;
+        }
+
+        public void DisplayProductDetails()
+        {
+            Console.WriteLine($"ProductId: {ProductId}");
+
+            Console.WriteLine($"ProductName: {ProductName}");
+            Console.WriteLine($"ProductPrice: {ProductPrice}");
+            Console.WriteLine($"ProductQty: {Qty}");
+        }
+
+    }
+
+
+    class program
+        {
+            static void Main(string[] args)
+            {
+                product product = new product(1);
+                product.ProductName = "Laptop";
+                product.ProductPrice = 1000000;
+                product.Qty = 10;
+
+
+
+            product.DisplayProductDetails();
+
+               
+
+        }
+
+    }
+}
